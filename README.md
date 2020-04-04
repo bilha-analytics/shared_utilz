d4fec6cefe7c8fa3121a891800689164d4e9ad2d
Bilha <32512727+bilha-analytics@users.noreply.github.com>
2020-04-04 22:43:48 +0300

    resetting - TODO: char Vs word Vs sent level impl

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Shared Utils
-Libs and tools used across different projects
+# ES Services
+
+- [N-gram MLP Text Classification](https://nbviewer.jupyter.org/github/bilha-analytics/es_services/blob/ad32dd33370325d2c144f8ded7e3ad13c4a72910/chat_bot_2/notebooks/round_2%20-%20basics%20-%20Text%20Classification.ipynb)
+
+- [Single RNN and Stacked RNNs Text Classification](https://nbviewer.jupyter.org/github/bilha-analytics/es_services/blob/0204a4d5e3f102bce5f885b81190930818fa4c36/chat_bot_2/notebooks/round%202%20-%20basics%20-%20RNN%20Models.ipynb)
+
+- [nCoV19 Chat bot EDA and Modelling](https://nbviewer.jupyter.org/github/bilha-analytics/es_services/blob/a4172244de41c036236cc07dbc02dd6afe7982a9/faq_chat_bot/notebooks/nCoV-19_faq_chat_bot.ipynb)
--- zdataset.py
+++ zdataset.py
@@ -9,6 +9,7 @@ refactor: class, encoders and selectors decouple
 import zlogger 
 import zdata_source
 
+from collections.abc import Iterable
 import string, re 
 import math
 import numpy as np 
@@ -35,7 +36,7 @@ ZLEVEL_SENT = 5
 
 '''
 Preprocessing, encoding and filtering helpers 
-TODO: word Vs char Vs sentense level
+TODO: word Vs char Vs sentense level << this needs thought!! see all the silly print lines; not even using logger :/
 '''
 
 '''
@@ -43,13 +44,14 @@ Convert text to lower + remove punctuations
 Input: a list of texts 
 return: a list of tokens 
 '''
-def cleanup_case_and_puncts(*text): 
-    tokenz = text
+def cleanup_case_and_puncts(text): 
     punctuations = dict( (ord(p), None) for p in string.punctuation )
-    # print(">>> {}".format( repr(tokenz) ) ) 
-    tokenz = " ".join( [ t for t in tokenz ] ) 
-    return np.array( nltk.word_tokenize( tokenz.lower().translate( punctuations) ) ) 
-
+    # print("IN>>> {}".format( repr(text) ) ) 
+    tokenz = " ".join( text ) if not isinstance(text, str) else text
+    # print("PUNKT>>> {}".format( repr(tokenz) ) ) 
+    tokenz =  nltk.word_tokenize( tokenz.lower().translate( punctuations) ) 
+    # print("PUNKT.FIN>>> {}".format( repr(tokenz) ) ) 
+    return  tokenz 
 '''
 Remove stop words 
 Input: 
@@ -57,19 +59,31 @@ Input:
     stop_wordz: default is nltk.english 
 return: a list of tokens 
 '''
-def cleanup_stopwords(*text, stop_wordz=None):
-    tokenz = text
+def cleanup_stopwords(text, stop_wordz=None):    
     stopz = stopwords.words('english') if stop_wordz is None else stop_wordz 
-    return np.array([w for w in tokenz if not w.lower() in stopz ] )
+    stopz = set( stopz )     
+    # print("\n\n>>>>> CLEAN and stop_words contains covid = {} ".format(
+    #         'covid' in stopz if stopz is not None else 'Is None'
+    #     ))
+    # print("stopz {}".format( repr(stopz) ) ) 
+    tokenz = " ".join( text ) if not isinstance(text, str) else text
+    # print("STOPZ>>> {}".format( repr(tokenz) ) ) 
+    tokenz = [w for w in nltk.word_tokenize( tokenz ) if w.lower() not in stopz ] 
+    # print("STOPZ.FIN>>> {}".format( repr(tokenz) ) ) 
+    return  tokenz
 
 '''
 Remove numerics 
 Input: a list of texts tokens 
 return: a list of tokens 
 '''
-def cleanup_numbers(*text, stop_wordz):
-    tokenz = text
-    return np.array([ w for w in tokenz if w.isalpha() ])
+def cleanup_numbers(text):    
+    # print("IN_NUMZ>>> {}".format( repr(text) ) ) 
+    tokenz = " ".join( text ) if not isinstance(text, str) else text    
+    tokenz = re.sub( '\d+', "", tokenz ) 
+    tokenz = nltk.word_tokenize( tokenz )
+    # print("NUMZ.FIN>>> {}".format( repr(tokenz) ) ) 
+    return tokenz 
 
 '''
 Clean up text: 
@@ -78,13 +92,13 @@ Input:
         :   what operations
 return: a list of tokens 
 '''
-def cleanup_text(*text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False):
-    tokenz =  cleanup_case_and_puncts( *text ) 
+def cleanup_text(text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False):
+    tokenz =  cleanup_case_and_puncts( text ) 
     if remove_stopwordz:
-        tokenz = cleanup_stopwords(tokenz, stop_wordz=stop_wordz) 
+        tokenz = cleanup_stopwords( tokenz, stop_wordz=stop_wordz) 
     if remove_numberz:
         tokenz = cleanup_numbers( tokenz ) 
-    return np.array(tokenz )
+    return tokenz
    
 '''
 Use WordNetLemmatizer english dict
@@ -92,14 +106,17 @@ Use WordNetLemmatizer english dict
 Input:  
 Output:  
 '''
-def lemmatizeTokens(*text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False):
-    tokenz = cleanup_text( *text, 
+def lemmatizeTokens(text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False):
+    tokenz = cleanup_text( text,  
         remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
         remove_numberz=remove_numberz) 
+    # print("LEMMAZ.IN>>> {}".format( repr(tokenz) ) ) 
 
     lemmatizer = nltk.stem.WordNetLemmatizer() 
+    tokenz = [ lemmatizer.lemmatize( token )  for token in tokenz ] 
+    # print("LEMMAZ.FIN>>> {}".format( repr(tokenz) ) ) 
 
-    return np.array( [ lemmatizer.lemmatize( token )  for token in tokenz ] )
+    return tokenz 
 
 
 '''
@@ -107,19 +124,25 @@ TODO: review
 Input:  
 Output:  
 '''
-def cleanup_and_lemmatize(*text, remove_stopwordz=False, stop_wordz=None, 
+def cleanup_and_lemmatize(text, remove_stopwordz=False, stop_wordz=None, 
                             remove_numberz=False, lemmatized=True, unique=False ): 
+
+    # print("\n\n>>>>> STARTING and stop_words contains covid = {} ".format(
+    #         'covid' in stop_wordz if stop_wordz is not None else 'Is None'
+    #     ))
     result = lemmatizeTokens( 
-                *text, 
+                text, 
                 remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
                 remove_numberz=remove_numberz
             ) if lemmatized else cleanup_text( 
-                *text, 
+                text, 
                 remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
                 remove_numberz=remove_numberz)
 
     if unique:
-        result = np.array( sorted( set(result ) )  )
+        result = sorted( set(result ) ) 
+
+    # print("<<<<< DONE CLEAN UP <<<< {}".format( repr(result) ) ) 
 
     return result 
 '''
@@ -128,28 +151,28 @@ word tokens without punct, lemmatize, get unique set
 Input:  
 Output:  
 '''
-def getVocabList(*text, remove_stopwordz=False, stop_wordz=None, 
+def getVocabList(text, remove_stopwordz=False, stop_wordz=None, 
                 remove_numberz=False, lemmatized=False):
-    vocab = cleanup_and_lemmatize( *text, 
+    vocab = cleanup_and_lemmatize( text, 
         remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
         remove_numberz=remove_numberz, lemmatized=lemmatized) 
-    return np.array( sorted( set( vocab ) ) )
+    return sorted( set( vocab ) ) 
 
 '''
 Operates
 Input:  
 Output: (vocab_vec, matrix) Vocab used and matrix per sentence encoded. 
 '''
-def onehotEncode(*text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False, lemmatized=False):
-    vocab = getVocabList( *text, 
+def onehotEncode(text, remove_stopwordz=False, stop_wordz=None, remove_numberz=False, lemmatized=False):
+    vocab = getVocabList( text, 
         remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
         remove_numberz=remove_numberz, lemmatized=lemmatized ) 
 
-    vect = onehotVectorizer(vocab, *text)
+    vect = onehotVectorizer(vocab, text)
 
     return vocab, vect
 
-def onehotVectorizer(vocab, *text):
+def onehotVectorizer(vocab, text):
     vect = []
     for ln in text:
         tmp = np.zeros( len(vocab) ) 
@@ -157,15 +180,15 @@ def onehotVectorizer(vocab, *text):
             if v in ln.lower() :
                 tmp[i] = 1
         vect.append( tmp )
-    return np.array( vect )
+    return vect
 
 '''
 Operates
 Input:  
 Output: (count_vectorizer, res_matrix) Vectorizer used and matrix per sentence encoded. 
 '''
-def countEncode(*text, ngram_range=(1,1), **kwargz ):
-    tokenz = cleanup_and_lemmatize( *text, 
+def countEncode(text, ngram_range=(1,1), **kwargz ):
+    tokenz = cleanup_and_lemmatize( text, 
             remove_stopwordz=kwargz['remove_stopwordz'], stop_wordz=kwargz['stop_wordz'], 
             remove_numberz=kwargz['remove_numberz'], lemmatized=kwargz['lemmatized'] )
     
@@ -180,12 +203,13 @@ Operates
 Input:  
 Output: (count_vectorizer, res_matrix) Vectorizer used and matrix per sentence encoded. 
 '''
-def tfidfEncode(*text, 
-        remove_stopwordz=False, stop_wordz=None, remove_numberz=False, lemmatized=False, 
+def tfidfEncode(text, 
+        remove_stopwordz=False, stop_wordz=None, 
+        remove_numberz=False, lemmatized=False, 
         ngram_range=(1,1)
         ):
 
-    tokenz = cleanup_and_lemmatize( *text, 
+    tokenz = cleanup_and_lemmatize(text, 
             remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
             remove_numberz=remove_numberz, lemmatized=lemmatized )
     
@@ -200,8 +224,9 @@ Operates
 Input:  
 Output: (count_vectorizer, res_matrix) Vectorizer used and matrix per sentence encoded. 
 '''
-def embeddingEncode(*text, 
-    remove_stopwordz=False, stop_wordz=None, remove_numberz=False, lemmatized=False, 
+def embeddingEncode(text, 
+    remove_stopwordz=False, stop_wordz=None, 
+    remove_numberz=False, lemmatized=False, 
     ngram_range=(1,1)
     ):
     raise NotImplementedError 
@@ -210,23 +235,23 @@ def embeddingEncode(*text,
 '''
 TODO:
 '''
-def doTrainEncode(enc_type, *text, **argz):
+def doTrainEncode(enc_type, text, **argz):
     params = ['remove_stopwordz', 'stop_wordz', 'remove_numberz','lemmatized']
     for p in params:
         if not p in argz.keys():
             argz[p] = None 
 
     encoder = dict_encoders.get(enc_type, ZENC_TFIDF) 
-    return encoder( *text, **argz)  
+    return encoder( text, **argz)  
 
 '''
 TODO: include preprocessing
 '''
-def doPredictEncode(enc_type, context, *text):
+def doPredictEncode(enc_type, context, text):
     result = None 
 
     if enc_type == ZENC_ONEHOT:
-        result = onehotVectorizer(context, *text)
+        result = onehotVectorizer(context, np.array(text) )
 
     elif enc_type == ZENC_COUNT or enc_type == ZENC_TFIDF:
         result = context.transform( text )        
@@ -238,7 +263,7 @@ def doPredictEncode(enc_type, context, *text):
 '''
 TODO: np.array Vs list consistent use 
 '''
-def splitTrainTest(*clean_data, test_prop=0.2): 
+def splitTrainTest(clean_data, test_prop=0.2): 
     the_data = np.array( clean_data ) 
     
     zlogger.log('splitTrainTest', "Provided data size = {}\n{}".format( len(the_data), the_data[0] ) ) 
@@ -278,20 +303,39 @@ TODO: abstract class, iterable, own and encap a data
 class ZDataset():
     ## TODO: overloaded constructor 
     ## TODO: expand multiple data in one return e.g. (train, test) or (faq_db, train)
-    def initFromSeq(self, *data):
-        self.data = data 
+    def initz(self):         
+        self.stop_wordz = None
+        self.clean_data = None
+        self.y_labelz = None 
+        # pass 
+
+    def initFromSeq(self, data):
+        self.initz()
+        self.data = np.array(data )
+
     def initFromResource(self, data_path, data_type): 
+        self.initz()
         self.load( data_path, data_type)
+
+    ####TODO: ownership 
+    def getStopWords(self, new_stop_words=None):
+        if self.stop_wordz is None:
+            self.stop_wordz = stopwords.words('english')      
+        if new_stop_words is not None:
+            self.stop_wordz.extend( new_stop_words) 
+        return self.stop_wordz 
 
     def load(self, data_path, data_type):
         self.data_path = data_path
         self.data_type = data_type 
-        self.data = zdata_source.readFrom(data_path, data_type) 
+        self.data = np.array( data_source.readFrom(data_path, data_type)  ) 
 
     def dumpSave(self, data_path=None, data_type=None):
         dpath = self.data_path if data_path is None else data_path 
         dtype = self.data_type if data_type is None else data_type
-        zdata_source.writeTo(self.data, dpath, dtype=dtype) 
+        zdata_source.writeTo(self.data, "{}.xftz".format(dpath), dtype=dtype) 
+        if self.y_labelz is not None:
+            zdata_source.writeTo(self.y_labelz, "{}.ylbz".format(dpath), dtype=dtype) 
 
     '''
     Can be called before or after preprocessing TODO: set as option 
@@ -305,13 +349,15 @@ class ZDataset():
     def splitIntoTrainTest(self, test_prop=0.2):
         db = self.clean_data if self.clean_data is not None else self.data
 
-        train, test = splitTrainTest(*db, test_prop) 
+        train, test = splitTrainTest(db, test_prop) 
         
         self.train_data = ZDataset()
-        self.train_data.initFromSeq(*train)
+        self.train_data.initFromSeq(train)
+        self.train_data.clean_data = self.train_data.data
 
         self.test_data = ZDataset()
-        self.test_data.initFromSeq(*test)
+        self.test_data.initFromSeq(test)
+        self.test_data.clean_data = self.test_data.data
 
         return self.train_data, self.test_data
 
@@ -325,27 +371,70 @@ class ZDataset():
         - remove numbers 
         - lemmatize 
     TODO: bag of words vs sequences
-    '''
-    def preprocess(self, remove_stopwordz=False, stop_wordz=None, remove_numberz=False, lemmatized=True, unique=False): 
-        self.clean_data = cleanup_and_lemmatize(*self.data, 
-            remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
-            remove_numberz=remove_numberz, lemmatized=lemmatized) 
-        if unique:
-            self.clean_data = self.getVocab( ) 
+    Efficient array op @ vectorize
 
+    TODO:
+    end state: 
+        Opt 1: a string/sentence of tokens <== let vectorizer decide
+        Opt 2: self.clean_data represents each observation as a list of tokens 
+    '''
+    def preprocess(self, remove_stopwordz=False, stop_wordz=None, 
+                    remove_numberz=False, lemmatized=True, unique=False): 
+        # tmp = np.vectorize(cleanup_and_lemmatize)( # 
+        self.clean_data  =  np.array( [ 
+                " ".join( cleanup_and_lemmatize(
+                        rec, 
+                        remove_stopwordz=remove_stopwordz, stop_wordz=stop_wordz, 
+                        remove_numberz=remove_numberz, lemmatized=lemmatized) )
+                for rec in self.data ] ) 
+                
+        if unique: #use vocab within a record 
+            self.clean_data = np.array([
+                " ".join( sorted( set( rec ) )   ) 
+                        for rec in self.clean_data
+            ])
+
+    '''
+    get vocab across entire data set
+    TODO: fix recomputes each time
+    ''' 
     def getVocab(self):
-        return np.array( sorted( set( self.clean_data ) ) ) 
+        dat = [
+                " ".join( sorted( set( nltk.word_tokenize(rec) ) ) ) 
+                        for rec in self.clean_data
+            ] 
+        # print("DSET.VOCAB.JOIN>>> {}".format( repr(dat) ) ) 
+        
+        dat = nltk.word_tokenize( " ".join(dat) )         
+        # print("DSET.VOCAB.TOKZ>>> {}".format( repr(dat) ) ) 
+
+        vocab = sorted( set( dat) )
+        # print("DSET.VOCAB.FIN>>> {}".format( repr(vocab) ) ) 
+
+        return  vocab  
+
+    ### TODO: major fix, too tired to make sense of it but there's a silly redundancy in here
+    def getWords(self):
+        dat = [
+                " ".join( sorted( set( nltk.word_tokenize(rec) ) ) ) 
+                        for rec in self.clean_data
+            ] 
+        dat = nltk.word_tokenize( " ".join(dat) )  
+
+        # print("DSET.WORDS.FIN>>> {}".format( repr(dat) ) )         
+
+        return  dat 
 
     '''
     TODO: expand options and train Vs test/predict
     '''
     def encodeTrain(self,  enc_type=None):       
-        self.context, self.encoded_matrix = doTrainEncode( enc_type, *self.clean_data  ) 
+        self.context, self.encoded_matrix = doTrainEncode( enc_type, self.clean_data  ) 
         self.enc_type = enc_type 
         return self.context, self.encoded_matrix 
 
-    def encodePredict(self, *text):
-        return doPredictEncode( self.enc_type, self.context,  *text) 
+    def encodePredict(self, text):
+        return doPredictEncode( self.enc_type, self.context,  text) 
         
     
 
@@ -356,7 +445,9 @@ class ZGsheetFaqDataSet( ZDataset ):
         self.data_path = data_path
         self.data_type = data_type 
         # zdata_source. returns dicts with class_cat as key         
-        self.faq_db, self.data = zdata_source.readFrom(data_path, data_type)  
+        self.faq_db, self.phrases_db = zdata_source.readFrom(data_path, data_type)  
+        self.data = np.array( [ k for k in self.phrases_db.keys() ] ) 
+        self.y_labelz = np.array( [ k for k in self.phrases_db.values() ] ) 
 
     '''
     b/c model will work with numerics, index the input phrases such that we can find their class_cat to look up the faq db
@@ -365,7 +456,8 @@ class ZGsheetFaqDataSet( ZDataset ):
     '''
     def category_to_index(self):
         # self.faq_db = { cat : resp for  (_, cat, resp, *_) in self.faq_db }    
-        self.faq_db = { cat : resp for  (cat, resp) in enumerate(self.faq_db) }    
+        # self.faq_db = { cat : resp for  (cat, resp) in enumerate(self.faq_db) } 
+        pass    
 
     def fetchResponse(self, class_category):
         return self.faq_db.get( class_category, "I don't know about that one yet. I'll go learn some more.")
